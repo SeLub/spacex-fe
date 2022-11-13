001b6aad7c6e5c420bb674b42ef20f266db6835a
Sergio <sergio@pop-os.localdomain>
2022-11-13 13:55:10 +0300

    refactor: remove comments

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,21 +1,8 @@
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
 import Image from '../assets/MGorDUi.png'
 import React from 'react'
 
 function Product() {
   return (
-//     <Card style={{ width: '18rem' }}>
-//       <Card.Img variant="top" src="holder.js/100px180" />
-//       <Card.Body>
-//         <Card.Title>Card Title</Card.Title>
-//         <Card.Text>
-//           Some quick example text to build on the card title and make up the
-//           bulk of the card's content.
-//         </Card.Text>
-//         <Button variant="primary">Go somewhere</Button>
-//       </Card.Body>
-//     </Card>
 
 <div className="card p-3">
         
